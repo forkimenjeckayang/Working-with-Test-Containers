cd6950962442cbc929106274b665b3b39da2c707
AssahBismarkabah <assah9799@gmail.com>
2024-04-02 23:18:05 +0100

    feat: added application-test

--- src/main/java/com/bismark/test/com/bismark/StudentController.java
+++ src/main/java/com/bismark/test/com/bismark/StudentController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
 
     @Autowired
-    private final StudentService studentService;
+    private StudentService studentService;
 
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
