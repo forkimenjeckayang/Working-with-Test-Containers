d781853fb73db4fc129b9435b7a5739441342bfe
AssahBismarkabah <assah9799@gmail.com>
2024-04-02 22:55:55 +0100

    feat: studentservice

---
+++ src/main/java/com/bismark/test/com/bismark/StudentService.java
@@ -0,0 +1,22 @@
+package com.bismark.test.com.bismark;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.bismark.test.com.bismark.*;
+
+
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public Student addStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+}
